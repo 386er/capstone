9453cf9be8b77bed403768ba28adf7fd465b1b44
System Administrator <root@tobiass-mbp.speedport_w_724v_09011603_05_010>
2017-06-11 23:07:15 +0200

    Add visualisation for metrics

--- helperModule.py
+++ helperModule.py
@@ -1,6 +1,8 @@
 from pandas import DataFrame, read_csv
 from sklearn import preprocessing
 import numpy as np
+from matplotlib import gridspec
+import matplotlib.pyplot as plt
 
 
 field_types = read_csv('./data/Field Names.csv', names=['field_name','field_type'])
@@ -120,8 +122,261 @@ def splitData(dataset):
 
 
 
+def computePerformanceMetrics(conf_benchmark, conf_model):
+    
+    metrics = {}
+    
+    accuracy_bench =np.sum([float(conf_benchmark[i][i])/np.sum(conf_benchmark) for i in range(5)])
+    accuracy_model =np.sum([float(conf_model[i][i])/np.sum(conf_model) for i in range(5)])
+
+    tpr_bench = [float(conf_benchmark[i][i])/(conf_benchmark.sum(axis=1)[i]) if conf_benchmark.sum(axis=1)[i] > 0 else float(conf_benchmark[i][i])/(conf_benchmark.sum(axis=0)[i] + 1)  for i,value in enumerate(conf_benchmark.sum(axis=1))]
+    tpr_model = [float(conf_model[i][i])/(conf_model.sum(axis=1)[i]) if conf_model.sum(axis=1)[i] > 0 else float(conf_model[i][i])/(conf_model.sum(axis=0)[i] + 1)  for i,value in enumerate(conf_model.sum(axis=1))]
+    
+    fpr_bench = [float((conf_benchmark.sum(axis=1)[i] - conf_benchmark[i][i])) / conf_benchmark.sum(axis=1)[i] if conf_benchmark.sum(axis=1)[i] > 0 else float((conf_benchmark.sum(axis=1)[i] - conf_benchmark[i][i])) / (conf_benchmark.sum(axis=1)[i] + 1 ) for i,value in enumerate(conf_benchmark.sum(axis=1))]
+    fpr_model = [float((conf_model.sum(axis=1)[i] - conf_model[i][i])) / conf_model.sum(axis=1)[i] if conf_model.sum(axis=1)[i] > 0 else float((conf_model.sum(axis=1)[i] - conf_model[i][i])) / (conf_model.sum(axis=1)[i] + 1 ) for i,value in enumerate(conf_model.sum(axis=1))]
+
+    prec_bench = [float(conf_benchmark[i][i])/(conf_benchmark.sum(axis=0)[i]) if conf_benchmark.sum(axis=0)[i] > 0 else float(conf_benchmark[i][i])/(conf_benchmark.sum(axis=0)[i] + 1)  for i,value in enumerate(conf_benchmark.sum(axis=0))]
+    prec_model = [float(conf_model[i][i])/(conf_model.sum(axis=0)[i]) if conf_model.sum(axis=0)[i] > 0 else float(conf_model[i][i])/(conf_model.sum(axis=0)[i] + 1)  for i,value in enumerate(conf_model.sum(axis=0))]
+    
+    f1_bench = [2*(prec_bench[i]*tpr_bench[i])/(prec_bench[i]+tpr_bench[i] + 0.00000001) for i in range(len(tpr_bench))]
+    f1_model = [2*(prec_model[i]*tpr_model[i])/(prec_model[i]+tpr_model[i] + 0.00000001) for i in range(len(tpr_model))]
+
+    
+    metrics['accuracy_bench'] = accuracy_bench
+    metrics['accuracy_model'] = accuracy_model
+
+    metrics['tpr_bench'] = tpr_bench
+    metrics['tpr_model'] = tpr_model
+    
+    metrics['fpr_bench'] = fpr_bench
+    metrics['fpr_model'] = fpr_model
+    
+    metrics['prec_bench'] = prec_bench
+    metrics['prec_model'] = prec_model
+    
+    metrics['f1_bench'] = f1_bench
+    metrics['f1_model'] = f1_model
+    
+    return metrics
 
 
+
+
+def plotMetrics(metrics):
+
+	fig = plt.figure(figsize=(18,8))
+	gs  = gridspec.GridSpec(2, 4, height_ratios=[0.3,1])
+	ax1 = plt.subplot(gs[0,  :])
+	ax2 = plt.subplot(gs[1, 0:1])
+	ax3 = plt.subplot(gs[1, 1:2])
+	ax4 = plt.subplot(gs[1, 2:3])
+	ax5 = plt.subplot(gs[1, 3:4])
+
+	x1 = [1,5,9,13,17]
+	x2 = [2,6,10,14,18]
+
+
+	ax1.barh([1], [metrics['accuracy_bench']], height=0.4)
+	ax1.barh([2], [metrics['accuracy_model']], height=0.4)
+	ax1.set_yticks([1,2])
+	ax1.set_yticklabels(['Bench','Model'])
+	ax1.set_xlabel('Accuracy')
+
+
+	ax2.bar(x1, metrics['tpr_bench'], label='Benchmark')
+	ax2.bar(x2, metrics['tpr_model'], label="Model")
+	ax2.set_xlabel('TPR')
+	ax2.set_xticks([1.5,5.5,9.5,13.5,17.5])
+	ax2.set_xticklabels([0,1,2,3,4])
+	ax2.legend()
+
+	ax3.bar(x1, metrics['fpr_bench'], label='Benchmark')
+	ax3.bar(x2, metrics['fpr_model'], label="Model")
+	ax3.set_xlabel('FPR')
+	ax3.set_xticks([1.5,5.5,9.5,13.5,17.5])
+	ax3.set_xticklabels([0,1,2,3,4])
+
+	ax4.bar(x1, metrics['prec_bench'], label='Benchmark')
+	ax4.bar(x2, metrics['prec_model'], label="Model")
+	ax4.set_xlabel('Precision')
+	ax4.set_xticks([1.5,5.5,9.5,13.5,17.5])
+	ax4.set_xticklabels([0,1,2,3,4])
+
+	ax5.bar(x1, metrics['f1_bench'], label='Benchmark')
+	ax5.bar(x2, metrics['f1_model'], label="Model")
+	ax5.set_xlabel('F1-Score')
+	ax5.set_xticks([1.5,5.5,9.5,13.5,17.5])
+	ax5.set_xticklabels([0,1,2,3,4])
+
+
+
+	plt.show()
+
+
+
+
+
+
+
+
+
+'''
+
+
+fig = plt.figure(figsize=(18,8))
+gs  = gridspec.GridSpec(2, 4, height_ratios=[0.3,1])
+ax1 = plt.subplot(gs[0,  :])
+ax2 = plt.subplot(gs[1, 0:1])
+ax3 = plt.subplot(gs[1, 1:2])
+ax4 = plt.subplot(gs[1, 2:3])
+ax5 = plt.subplot(gs[1, 3:4])
+
+x1 = [1,5,9,13,17]
+x2 = [2,6,10,14,18]
+
+
+ax1.barh([1], [accuracy_bench], height=0.4)
+ax1.barh([2], [val_accuracy], height=0.4)
+ax1.set_yticks([1,2])
+ax1.set_yticklabels(['Bench','Model'])
+ax1.set_xlabel('Accuracy')
+
+
+ax2.bar(x1, metrics['tpr_bench'], label='Benchmark')
+ax2.bar(x2, metrics['tpr_model'], label="Model")
+ax2.set_xlabel('TPR')
+ax2.set_xticks([1.5,5.5,9.5,13.5,17.5])
+ax2.set_xticklabels([0,1,2,3,4])
+ax2.legend()
+
+ax3.bar(x1, metrics['fpr_bench'], label='Benchmark')
+ax3.bar(x2, metrics['fpr_model'], label="Model")
+ax3.set_xlabel('FPR')
+ax3.set_xticks([1.5,5.5,9.5,13.5,17.5])
+ax3.set_xticklabels([0,1,2,3,4])
+
+ax4.bar(x1, metrics['prec_bench'], label='Benchmark')
+ax4.bar(x2, metrics['prec_model'], label="Model")
+ax4.set_xlabel('Precision')
+ax4.set_xticks([1.5,5.5,9.5,13.5,17.5])
+ax4.set_xticklabels([0,1,2,3,4])
+
+ax5.bar(x1, metrics['f1_bench'], label='Benchmark')
+ax5.bar(x2, metrics['f1_model'], label="Model")
+ax5.set_xlabel('F1-Score')
+ax5.set_xticks([1.5,5.5,9.5,13.5,17.5])
+ax5.set_xticklabels([0,1,2,3,4])
+
+
+
+plt.show()
+
+
+'''
+
+
+
+
+
+
+
+
+
+'''
+
+
+tpr = [float(conf_matrix[i][i])/(conf_matrix.sum(axis=1)[i]) if conf_matrix.sum(axis=1)[i] > 0 else float(conf_matrix[i][i])/(conf_matrix.sum(axis=0)[i] + 1)  for i,value in enumerate(conf_matrix.sum(axis=1))]
+
+print 'TPR NORMAL %.2f %%' % (tpr[0] * 100)
+print 'TPR PROBE  %.2f %%' % (tpr[1] * 100)
+print 'TPR DOS    %.2f %%' % (tpr[2] * 100)
+print 'TPR U2R    %.2f %%' % (tpr[3] * 100)
+print 'TPR R2L    %.2f %%' % (tpr[4] * 100)
+
+print '************'
+
+fpr = [float((conf_matrix.sum(axis=1)[i] - conf_matrix[i][i])) / conf_matrix.sum(axis=1)[i] if conf_matrix.sum(axis=1)[i] > 0 else float((conf_matrix.sum(axis=1)[i] - conf_matrix[i][i])) / (conf_matrix.sum(axis=1)[i] + 1 ) for i,value in enumerate(conf_matrix.sum(axis=1))]
+
+print 'FPR NORMAL %.2f %%' % (fpr[0] * 100)
+print 'FPR PROBE  %.2f %%' % (fpr[1] * 100)
+print 'FPR DOS    %.2f %%' % (fpr[2] * 100)
+print 'FPR U2R    %.2f %%' % (fpr[3] * 100)
+print 'FPR R2L    %.2f %%' % (fpr[4] * 100)
+
+print '************'
+
+precision = [float(conf_matrix[i][i])/(conf_matrix.sum(axis=0)[i]) if conf_matrix.sum(axis=0)[i] > 0 else float(conf_matrix[i][i])/(conf_matrix.sum(axis=0)[i] + 1)  for i,value in enumerate(conf_matrix.sum(axis=0))]
+
+print 'Precision NORMAL %.2f %%' % (precision[0] * 100)
+print 'Precision PROBE  %.2f %%' % (precision[1] * 100)
+print 'Precision DOS    %.2f %%' % (precision[2] * 100)
+print 'Precision U2R    %.2f %%' % (precision[3] * 100)
+print 'Precision R2L    %.2f %%' % (precision[4] * 100)
+
+print '************'
+
+f1_score = [2*(precision[i]*tpr[i])/(precision[i]+tpr[i] + 0.0000001) for i in range(len(tpr))]
+
+print 'F1_score NORMAL %.2f %%' % (f1_score[0] * 100)
+print 'F1_score PROBE  %.2f %%' % (f1_score[1] * 100)
+print 'F1_score DOS    %.2f %%' % (f1_score[2] * 100)
+print 'F1_score U2R    %.2f %%' % (f1_score[3] * 100)
+print 'F1_score R2L    %.2f %%' % (f1_score[4] * 100)
+
+
+
+
+fig = plt.figure(figsize=(18,4))
+	gs  = gridspec.GridSpec(2, 4, height_ratios=[1])
+	ax1 = plt.subplot(gs[0,  :])
+	ax2 = plt.subplot(gs[1, 0:1])
+	ax3 = plt.subplot(gs[1, 1:2])
+	ax4 = plt.subplot(gs[1, 2:3])
+	ax5 = plt.subplot(gs[1, 3:4])
+
+	x1 = [1,5,9,13,17]
+	x2 = [2,6,10,14,18]
+	y = [2,6,8,3,2]
+
+
+	ax1.bar(x1, metrics['tpr_bench'], label='Benchmark')
+	ax1.bar(x2, metrics['tpr_model'], label="Model")
+	ax1.set_xlabel('TPR')
+	ax1.set_xticks([1.5,5.5,9.5,13.5,17.5])
+	ax1.set_xticklabels([0,1,2,3,4])
+	ax1.legend()
+
+	ax2.bar(x1, metrics['fpr_bench'], label='Benchmark')
+	ax2.bar(x2, metrics['fpr_model'], label="Model")
+	ax2.set_xlabel('FPR')
+	ax2.set_xticks([1.5,5.5,9.5,13.5,17.5])
+	ax2.set_xticklabels([0,1,2,3,4])
+
+	ax3.bar(x1, metrics['prec_bench'], label='Benchmark')
+	ax3.bar(x2, metrics['prec_model'], label="Model")
+	ax3.set_xlabel('Precision')
+	ax3.set_xticks([1.5,5.5,9.5,13.5,17.5])
+	ax3.set_xticklabels([0,1,2,3,4])
+
+	ax4.bar(x1, metrics['f1_bench'], label='Benchmark')
+	ax4.bar(x2, metrics['f1_model'], label="Model")
+	ax4.set_xlabel('F1-Score')
+	ax4.set_xticks([1.5,5.5,9.5,13.5,17.5])
+	ax4.set_xticklabels([0,1,2,3,4])
+
+
+
+	plt.show()
+
+
+
+
+
+
+
+
+
+'''
 
 
 
